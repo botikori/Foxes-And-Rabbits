92d0b1297a31c483071ddaf8c7cd9c6ed30a14e4
botikori <54451878+botikori@users.noreply.github.com>
2023-09-27 21:20:50 +0200

    Number check added in Program.cs

--- FoxesAndRabbits/FoxesAndRabbits/Program.cs
+++ FoxesAndRabbits/FoxesAndRabbits/Program.cs
@@ -6,12 +6,30 @@ int rows;
 do
 {
     Console.Write("#1 Oszlopok száma(10-20): ");
-    columns = int.Parse(Console.ReadLine());
+    
+    bool succes = int.TryParse(Console.ReadLine(), out columns);
+
+    if (!succes) Console.WriteLine("Számot kell megadni!");
+    else
+    {
+        if (columns < 10) Console.WriteLine("10-nél nagyobb számot kell megadni!");
+        if (columns > 20) Console.WriteLine("20-nál kisebb számot kell megadni!");
+    }
 } while (columns<10 || columns > 20);
+
 do
 {
     Console.Write("#2 Sorok száma(10-20): ");
-    rows= int.Parse(Console.ReadLine());
+    
+    bool succes = int.TryParse(Console.ReadLine(), out rows);
+
+    if (!succes) Console.WriteLine("Számot kell megadni!");
+    else
+    {
+        if (rows < 10) Console.WriteLine("10-nél nagyobb számot kell megadni!");
+        if (rows > 20) Console.WriteLine("20-nál kisebb számot kell megadni!");
+    }
+
 } while (rows<10 || rows > 20);
 
 
